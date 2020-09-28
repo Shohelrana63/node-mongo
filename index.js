e6493f148aef4d4a668388640a535f9e184db551
Shohel Rana <39130772+Shohelrana63@users.noreply.github.com>
2020-09-28 10:46:16 +0600

    done this module

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+const users = ['jahid', 'asad', 'moin', 'jahid', 'abir', 'john', 'alif', 'habu']
 
 app.get('/', (req, res) => {
-    res.send("i know how to open node..YAY");
+    res.send("Node ami onk pari..YAY");
 })
-app.listen(3000, () => console.log('listening port 3000'))
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const name = users[id];
+    res.send({ id, name });
+})
+//POST
+app.post('/addUser', (req, res) => {
+    //SAVE TO DATABASE
+    const user = req.body;
+    user.id = 55;
+    res.send(user);
+
+})
+app.listen(4000, () => console.log('listening port 4000'))
